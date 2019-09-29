305e0e0dbaead7c8b728f97a8f83e20045860db2
Davi Alves <davi.sousa10@gmail.com>
2019-09-28 23:00:09 -0300

    Done

--- README.md
+++ README.md
@@ -1,18 +1,20 @@
-#Requisitos
+# Requisitos
 
 1.  Você deverá ter instalado terminal do git
 1.  Você deverá ter instalado o docker
 
-#Instalação
+# Instalação
 
-1.  Link para fazer a instalação do terminal do git no windows <https://git-scm.com/download/win>, caso esteja usando
- um sistema operacional linux o git provavelmente já estará instalado, caso contrário você podera encontrar tutorias
-  ao pesquisa algo como `install git on {current OS}`
+1.  Link para fazer a instalação do terminal do git no windows <https://git-scm.com/download/win>, caso esteja usando um sistema operacional linux o git provavelmente já estará instalado, caso contrário você podera encontrar tutorias
+    ao pesquisa algo como `install git on {current OS}`
 2.  Para instalar o dokcer você pode ir nesse link e efetuar o processo de instalação de acordo com seu sistema
- operacional [link](https://docs.docker.com/install/) 
+    operacional [link](https://docs.docker.com/install/)
 
-#Instruções
-3. Feito as etapas anteriores, podemos partir para próxima etapa que é clonar o repositório em sua máquina, copie esse
- trecho em seu terminal do git `git clone https://github.com/Davizera/desafio-focomultimidia.git`, após finalizado o comando terá sido criado uma pasta com o nome `desafio-focomultimidia` no diretório corrente entre nela usando o `cd desafio-focomultimidia`
- 4. Então agora estamos habilitados a usar o comando `docker run --rm --name my-running-script -v "$PWD":/usr/src/myapp
-  -w /usr/src/myapp php:7.2-cli php hotel-desafio.php`  para executar o script com o algoritmo do desafio  
+# Instruções
+
+1. Feito as etapas anteriores, podemos partir para próxima etapa que é clonar o repositório em sua máquina, copie esse
+   trecho em seu terminal do git `git clone https://github.com/Davizera/desafio-focomultimidia.git`
+2. Após finalizado o comando, terá sido criado uma pasta com o nome `desafio-focomultimidia` no diretório corrente entre nela usando o `cd desafio-focomultimidia`
+3. Então agora estamos habilitados a usar o comando `[sudo] (caso esteja usando ambiente linux) docker run --rm --name my-running-script -v "$PWD":/usr/src/myapp -w /usr/src/myapp php:7.2-cli php hotel-desafio.php` para executar o script com o algoritmo do desafio
+4. O script php será executado em seu terminal então olhe a saída gera
+5. (Opcional) Caso queira verificar o código feito execute o comando `cat hotel-desafio.php`
--- hotel-desafio.php
+++ hotel-desafio.php
@@ -1,8 +1,218 @@
 <?php
 
-$quartoIndvidual = 100.00;
-$quartoDuplo = 150.00;
-$taxaServico = 0.05;
-$taxaTurismo = 10.00;
-$taxaWifi = 20.00;
+/**
+ * No caso em questão, foi considerado que Kleber (cliente) contratou o quarto individual e não fez uso do wifi.
+ * Foram criadas funções para poder mudar o funcionamento do código como setar que o cliente contratou o wifi, quantos dias de uso fez do wifi, quantas pessoas estavam no quarto com ele e qual o tipo de quarto reservado, sendo fixado a taxa do turismo. 
+ */
 
+class Cliente
+{
+
+    private $name;
+    private $tipoQuarto = 1; # valores entre 1 (individual) e 2 (duplo)
+    private $diaEntrada;
+    private $diaSaida;
+    private $estadia;
+    private $usouWifi = 0;
+    private $usoWifiDias = 0;
+
+    /**
+     * construct
+     *
+     * @param [string] $name
+     * @param [int] $tipoQuarto
+     * @param [string] $diaEntrada
+     * @param [string] $diaSaida
+     */
+    public function __construct($name, $tipoQuarto, $diaEntrada, $diaSaida)
+    {
+        $this->name = $name;
+        $this->tipoQuarto = $tipoQuarto;
+        $this->diaEntrada = new DateTime($diaEntrada, new DateTimeZone('America/Bahia'));
+        $this->diaSaida = new DateTime($diaSaida, new DateTimeZone('America/Bahia'));
+        $this->estadia = $this->diaEntrada->diff($this->diaSaida);
+    }
+
+    /**
+     * getName
+     *
+     * @param [string] $name
+     * @return void
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * setTipoQuarto
+     *
+     * @param [int] $tipoQuarto
+     * @return void
+     */
+    public function setTipoQuarto($tipoQuarto)
+    {
+        $this->tipoQuarto = $tipoQuarto;
+    }
+
+    /**
+     * setUsouWifi
+     *
+     * @param [int] $usouWifi
+     * @return void
+     */
+    public function setUsouWifi($usouWifi)
+    {
+        $this->usouWifi = $usouWifi;
+    }
+
+    /**
+     * setUsouWifiDias
+     *
+     * @param [string] $usoWifiDias tem que estar no formato aceitado pelao DateInterval
+     * @return void
+     */
+    public function setUsoWifiDias($usoWifiDias)
+    {
+        $this->usoWifiDias = new DateInterval($usoWifiDias);
+    }
+
+    /**
+     * setDiaEntrada
+     *
+     * @param [string] $diaEntrada tem que estar no formato aceitado pelo DateTime
+     * @return void
+     */
+    public function setDiaEntrada($diaEntrada)
+    {
+        $this->diaEntrada = new DateTime($diaEntrada, new DateTimeZone('America/Bahia'));
+    }
+
+    /**
+     * setDiaSaida
+     *
+     * @param [string] $diaSaida tem que estar no formato aceitado pelo DateTime
+     * @return void
+     */
+    public function setDiaSaida($diaSaida)
+    {
+        $this->diaSaida = new DateTime($diaSaida, new DateTimeZone('America/Bahia'));
+    }
+    /**
+     * getName
+     *
+     * @return [string]
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+    /**
+     * getDiaEntrada
+     *
+     * @return [string]
+     */
+    public function getDiaEntrada()
+    {
+        return $this->diaEntrada->format('d/m/y');
+    }
+    /**
+     * getEstadia
+     *
+     * @return [string]
+     */
+    public function getEstadia()
+    {
+        return $this->estadia->days;
+    }
+    /**
+     * getDiaSaida
+     *
+     * @return [string]
+     */
+    public function geDiaSaida()
+    {
+        return $this->diaSaida->format('d/m/y');
+    }
+
+    /**
+     * getUsouWifi
+     *
+     * @return [boolean]
+     */
+    public function getUsouWifi()
+    {
+        return !!$this->usouWifi;
+    }
+
+    /**
+     * getUsoWifiDias
+     *
+     * @return [string]
+     */
+    public function getUsoWifiDias()
+    {
+        return $this->usoWifiDias->d;
+    }
+
+    /**
+     * getTipoQuarto
+     *
+     * @return [int]
+     */
+    public function getTipoQuarto()
+    {
+        return $this->tipoQuarto;
+    }
+}
+class Hotel
+{
+
+    private const QUARTO_INDIVIDUAL = 100.00;
+    private const QUARTO_DUPLO = 150.00;
+    private const TAXA_SERVICO = 0.05;
+    private const TAXA_TURISMO = 10.00;
+    private const TAXA_WIFI = 20.00;
+    private $pessoasNoQuarto = 1; # no mínimo tera uma pessoa no quarto o mesmo for reservado
+
+    /**
+     * getPessoasNoQuarto
+     *
+     * @return [int]
+     */
+    public function getPessoasNoQuarto()
+    {
+        return $this->pessoasNoQuarto;
+    }
+
+    /**
+     * calculaValorFinal
+     * Calcula o valor a ser pago pelo clinte que fez uso do quarto
+     * 
+     * @param Cliente $cliente
+     * @return [float]
+     */
+    public function calculaValorFinal(Cliente $cliente)
+    {
+        if ($cliente->getTipoQuarto() == 1) {
+            return ($cliente->getEstadia() * $this::QUARTO_INDIVIDUAL)
+                + ($cliente->getEstadia() * ($this::TAXA_SERVICO * $this::QUARTO_INDIVIDUAL))
+                + ($cliente->getEstadia() * $this::TAXA_TURISMO)
+                + ($cliente->getEstadia() * $this::TAXA_TURISMO * $this->getPessoasNoQuarto())
+                + ($this->usouWifi ? $this->usoWifiDias * $this::TAXA_WIFI : 0);
+        } else {
+            return ($cliente->getEstadia() * $this::QUARTO_DUPLO)
+                + ($cliente->getEstadia() * ($this::TAXA_SERVICO * $this::QUARTO_INDIVIDUAL))
+                + ($cliente->getEstadia() * $this::TAXA_TURISMO)
+                + ($cliente->getEstadia())
+                + ($cliente->getEstadia() * $this::TAXA_TURISMO * $this->getPessoasNoQuarto())
+                + ($cliente->getUsouWifi() ? $cliente->usoWifiDias() * $this::TAXA_WIFI : 0);
+        }
+    }
+}
+
+$cliente = new Cliente('Klebler', 1, '24-12-2019', '27-12-2019');
+$hotel = new Hotel();
+
+echo "O valor pago por {$cliente->getName()} será de R$" . floatval($hotel->calculaValorFinal($cliente));
+echo "\n";
